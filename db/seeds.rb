f2293695475a7bbeee2f5a77945840676d09864c
Yuchi Hsueh <bhsueh94@gmail.com>
2022-08-17 18:02:14 -0300

    styled

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3,
+  picture_url: 'https://images.unsplash.com/photo-1448630360428-65456885c650?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1167&q=80'
+)
+Flat.create!(
+  name: 'Great apartment in Paris',
+  address: 'Palermo 3435',
+  description: 'Great place to watch the Eiffel Tower from the balcony',
+  price_per_night: 100,
+  number_of_guests: 4,
+  picture_url: 'https://images.unsplash.com/photo-1503174971373-b1f69850bded?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1213&q=80'
+)
+Flat.create!(
+  name: 'Huge house at the LA suburbs',
+  address: '1st Street 3435',
+  description: 'Big house with pool and backyard',
+  price_per_night: 120,
+  number_of_guests: 8,
+  picture_url: 'https://images.unsplash.com/photo-1613490493576-7fde63acd811?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80'
+)
+Flat.create!(
+  name: 'Little downtown flat in the middle of Palermo, Buenos Aires',
+  address: 'Gorriti 1234',
+  description: 'Beautiful flat at the center of the night life of Buenos Aires',
+  price_per_night: 50,
+  number_of_guests: 2,
+  picture_url: 'https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1080&q=80'
+)
